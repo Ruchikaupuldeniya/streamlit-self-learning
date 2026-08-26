a69987001693cd6ec42cfa1d6564f6088b66cc16
Ruchika Upuldeniya <ruchikaupuldeniya14@gmail.com>
2026-08-26 22:28:37 +0530

    Add Reference: Phase 1 Streamlit Basics code

---
+++ Reference/Phase 1/Streamlit_Basics.py
@@ -0,0 +1,15 @@
+"""
+Docstring for Phase 1.StreamlitBasics => Text display
+"""
+
+import streamlit as st
+
+st.title("Title function")
+st.header("header function")
+st.subheader("subheader function")
+st.caption("caption function")
+st.text("text function")
+st.write("write function")
+st.latex("latex function")
+st.code("code function")
+st.divider()
